171234a10fa3bafe6a624c982c5822b37cec53ae
Jagadeesh Kunda <kundajagadeesh132@gmail.com>
2026-07-01 14:12:01 +0530

    Updated frontend API to production backend

--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL: "http://localhost:5000/api/tasks"
+baseURL: "baseURL: "https://task-tracker-xi-jade.vercel.app/api/tasks""
 });
 
 export default API;
